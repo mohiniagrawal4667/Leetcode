a77c19686634dc7e72d884c142dd7c508619a845
Mohini Agrawal <mohiniagrawal.4667@gmail.com>
2026-09-16 15:26:43 +0530

    Time: 3 ms (85.19%), Space: 48.3 MB (45.61%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+    int rows=0;
+    int cols=matrix[0].length-1;
+    while(rows<matrix.length && cols>=0){
+        if(matrix[rows][cols] == target){
+            return true;
+        }
+        else if(matrix [rows][cols] > target)
+            cols--;
+        else
+            rows++;
+    }
+        return false;
+    }
+}
